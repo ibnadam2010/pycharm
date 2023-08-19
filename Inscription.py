cbd85eb9a8aea7e06d833e4ea00d704dd9636076
Adamou Salifou <ibnadam2010@yahoo.fr>
2023-08-19 03:43:03 +0200

    first python application project

---
+++ Inscription.py
@@ -0,0 +1,12 @@
+from tkinter import ttk
+from tkinter import *
+from  tkinter import messagebox
+import sqlite3
+
+#titre general window
+fenetre = Tk()
+fenetre.title("Gestion des inscriptions")
+fenetre.geometry("1200x680+70+0")
+fenetre.configure(background ="#e9e9e9")
+
+fenetre.mainloop()
